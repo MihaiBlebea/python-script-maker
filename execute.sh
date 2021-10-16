86877010d9978b0c9e06dc9e594fd8eecb7014a6
Mihai Blebea <mihaiserban.blebea@gmail.com>
2021-10-16 19:35:42 +0100

    Fix typos

--- execute.sh
+++ execute.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-BIN_PATH="${HOME}/.local/bin/_scriptmaker"
-eval "${BIN_PATH}/virtualenv/bin/python3 ${BIN_PATH}/project.py \"${@}\""
+eval "${PWD}/virtualenv/bin/python3 ${PWD}/project.py \"${@}\""
