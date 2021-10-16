76f323e971acf1e5e5ee7bd1fe8fd4e27fb6efef
Mihai Blebea <mihaiserban.blebea@gmail.com>
2021-10-16 18:02:45 +0100

    Adds dinamic github url

--- installer.sh
+++ installer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+GIT_URL=$(git config --get remote.origin.url)
+
 if [[ $* == *-u* ]]; then
 	echo "uninstalling the scriptmaker"
 	unlink ${HOME}/.local/bin/scriptmaker && \
 	rm -rf ${HOME}/.local/bin/_scriptmaker
 else
 	echo "installing the scriptmaker"
-	git clone https://github.com/MihaiBlebea/python-script-maker && \
+	git clone ${GIT_URL} && \
 	cp -r ./python-script-maker/ ${HOME}/.local/bin/_scriptmaker/ && \
 	python3 -m venv ${HOME}/.local/bin/_scriptmaker/virtualenv && \
 	ln -s ${HOME}/.local/bin/_scriptmaker/execute.sh ${HOME}/.local/bin/scriptmaker
